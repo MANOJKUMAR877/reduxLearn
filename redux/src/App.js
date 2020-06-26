7668d1ff9b0f902f7fe60464b89c34a1b5a143f5
manojkumar <manoj.kumar@innoventes.co>
2020-06-26 12:45:45 +0530

    Multiple checkbox in react using new type

--- redux/src/App.js
+++ redux/src/App.js
@@ -1,81 +1,102 @@
 import React, { Component } from "react";
-    import Checkbox from "./Checkbox";
+import Checkbox from "./Checkbox";
 
-    class App extends Component {
-        constructor(props) {
-            super(props);
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-            this.state = {
-                checkedItems: new Map()
-            };
+    this.state = {
+      checkedItems: new Map(),
+      values:{}
+    };
 
-            this.handleChange = this.handleChange.bind(this);
-        }
+    this.handleChange = this.handleChange.bind(this);
+  }
+  submit=()=>{
+    this.setState({values:this.state.checkedItems},console.log(this.state.values))
+  }
 
-        handleChange = e => {
-            const item = e.target.name;
-            const isChecked = e.target.checked;
-            this.setState(prevState => ({
-                checkedItems: prevState.checkedItems.set(item, isChecked)
-            }));
-        };
+  handleChange = e => {
+    const item = e.target.name;
+    const isChecked = e.target.checked;
+    this.setState(prevState => ({
+      checkedItems: prevState.checkedItems.set(item, isChecked)
+    }),()=>{console.log(this.state.checkedItems)});
+  };
 
-        deleteCheckboxState = (name, checked) => {
-            const updateChecked = typeof checked === "undefined" ? true : false;
-            this.setState(prevState => prevState.checkedItems.set(name, updateChecked));
-        };
+  deleteCheckboxState = (name, checked) => {
+    const updateChecked = typeof checked === "undefined" ? true : false;
+    this.setState(prevState => prevState.checkedItems.set(name, updateChecked));
+  };
 
-        clearAllCheckboxes = () => {
-            const clearCheckedItems = new Map();
-            this.setState({ checkedItems: clearCheckedItems });
-        };
+  clearAllCheckboxes = () => {
+    const clearCheckedItems = new Map();
+    this.setState({ checkedItems: clearCheckedItems });
+  };
 
-        render() {
-            const checkboxes = [
-                {
-                    name: "check-box-1",
-                    key: "checkBox1",
-                    label: "Check Box 1"
-                },
-                {
-                    name: "check-box-2",
-                    key: "checkBox2",
-                    label: "Check Box 2"
-                },
-                {
-                    name: "check-box-3",
-                    key: "checkBox3",
-                    label: "Check Box 3"
-                },
-                {
-                    name: "check-box-4",
-                    key: "checkBox4",
-                    label: "Check Box 4"
-                }
-            ];
+  render() {
+    const checkboxes = [
+      {
+        name: "Manojkumar",
+        key: "checkBox1",
+        label: "Check Box 1"
+      },
+      {
+        name: "Hari Krishna",
+        key: "checkBox2",
+        label: "Check Box 2"
+      },
+      {
+        name: "Baskaran",
+        key: "checkBox3",
+        label: "Check Box 3"
+      },
+      {
+        name: "Uma",
+        key: "checkBox4",
+        label: "Check Box 4"
+      }
+    ];
 
-            const checkboxesToRender = checkboxes.map(item => {
-                return (
-                    <label key={item.key}>
-                        {item.name}
-                        <Checkbox
-                            name={item.name}
-                            checked={!!this.state.checkedItems.get(item.name)}
-                            onChange={this.handleChange}
-                            type="checkbox"
-                        />
-                    </label>
-                );
-            });
+    const checkboxesToRender = checkboxes.map(item => {
+      return (
+        <label key={item.key}>
+          {item.name}
+          <Checkbox
+            name={item.name}
+            checked={!!this.state.checkedItems.get(item.name)}
+            onChange={this.handleChange}
+            type="checkbox"
+          />
+        </label>
+      );
+    });
+    const checkboxesDeleteHandlers = checkboxes.map(item => {
+      return (
+        <span
+          key={item.name}
+          onClick={() =>
+            this.deleteCheckboxState(
+              item.name,
+              this.state.checkedItems.get(item.name)
+            )
+          }
+        >
+          {item.name} &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+        </span>
+      );
+    });
+    return (
+      <form className="App" onSubmit={this.submit} >
+        {checkboxesToRender}
+        <hr />
+        {checkboxesDeleteHandlers}
+        <button onClick={this.clearAllCheckboxes}>clear all</button>
+        <br/>
+        <button type="submit"> Submit</button>
+      </form>
+    );
+  }
+}
 
-            return (
-                <div className="App">
-                    {checkboxesToRender}
-                    <hr /> 
-                    <button onClick={this.clearAllCheckboxes}>clear all</button>
-                </div>
-            );
-        }
-    }
-
-    export default App;
+export default App;
